e6379c388c0b7a47382cbea4e0fb118b797e2a74
Javier Muñoz <123673773+javmunmay@users.noreply.github.com>
2023-09-22 10:05:46 +0200

    actualizacion de ejercicios

---
+++ ejercicios/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Introduce un número entero: ");
+        Scanner teclado = new Scanner(System.in);
+        int numero =teclado.nextInt();
+        System.out.println("El número introducido es: ");
+        System.out.println(numero);
+
+
+    }
+}
