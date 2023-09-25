edabde1a6cd2a011534a7e679a5e01a73189055e
Javier Muñoz <123673773+javmunmay@users.noreply.github.com>
2023-09-25 21:35:28 +0200

    Ejercicios de tarea4 ej4y5 completados

--- ejercicios/src/main/java/U1/tarea4/ejercicio4.java
+++ ejercicios/src/main/java/U1/tarea4/ejercicio4.java
@@ -1,7 +1,27 @@
 package U1.tarea4;
 
+import java.util.Scanner;
+
 public class ejercicio4 {
 
+    public static void main(String[] args) {
 
+        //Sería interesante disponer de un programa que pida como entrada un número decimal y
+        //lo muestre redondeando al entero más próximo. ej: 2,3 -> 2 4,8 -> 5
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Introduce un numero decimal: ");
+
+        double numeroDecimal =teclado.nextDouble();
+
+        int numeroRedondeado = (int) Math.round(numeroDecimal);
+
+        //Resultado redondeo
+        System.out.println("El numero redondeado es: "+numeroRedondeado);
+
+
+
+    }
 
 }
---
+++ ejercicios/src/main/java/U1/tarea4/ejercicio5.java
@@ -0,0 +1,16 @@
+package U1.tarea4;
+
+import java.time.LocalDateTime;
+
+public class ejercicio5 {
+
+    public static void main(String[] args) {
+
+        //Escribe un programa que muestre la hora actual
+
+        LocalDateTime horaLocal = LocalDateTime.now();
+
+        System.out.println("La hora actual es: "+ horaLocal);
+
+    }
+}
