86494fc3fd300fe5e282057935b749b429fc8da1
DAW Mañana <javi15mmj@gmail.com>
2023-11-17 08:36:04 +0100

    actualizacion

--- ejercicios/src/main/java/U3/tarea1Funciones/Ej1.java
+++ ejercicios/src/main/java/U3/tarea1Funciones/Ej1.java
@@ -4,7 +4,7 @@ public class Ej1 {
     public static void main(String[] args) {
         // Diseñar una función eco() a la que se le pasa como parámetro un número n, y muestra por pantalla n veces el mensaje "Eco..."
 
-    
+
 
 
 
